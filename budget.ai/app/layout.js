8681b9e2188eee3f4a525c0eadf6044b72fadfa1
Kamron Arabi <kamronarabi@gmail.com>
2025-07-05 17:51:47 -0400

    First commit

---
+++ budget.ai/app/layout.js
@@ -0,0 +1,35 @@
+import { ClerkProvider } from "@clerk/nextjs";
+import Header from "@/components/Header";
+import "./globals.css";
+import {Inter} from 'next/font/google'
+
+const inter = Inter({subsets:['latin']})
+
+export const metadata = {
+  title: "Budget.ai",
+  description: "One Stop Finance Platform",
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <ClerkProvider  appearance={{
+      cssLayerName: 'clerk',     
+    }}>
+      <html lang="en">
+        <body
+          className={`${inter.className}`}
+        >
+          <Header/>
+          <main className="min-h-screen">
+            {children}
+          </main>
+          <footer className="bg-blue-50 py-12">
+            <div className="container mx-auto px-4 text-center text-gray-600">
+              <p>Made by Kamron Arabi</p>
+            </div>
+          </footer>
+        </body>
+      </html>
+    </ClerkProvider>
+  );
+}
---
+++ budget.ai/app/page.js
@@ -0,0 +1,8 @@
+import {Button} from '@/components/ui/button'
+export default function Home() {
+  return (
+    <div className='mt-40'>
+      <Button variant="destructive"> Subscribe to Roadside Nigger </Button>
+    </div>
+  );
+}
