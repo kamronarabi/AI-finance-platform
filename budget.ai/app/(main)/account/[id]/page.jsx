4fa97429b166b17babf26589162ee21c6f995d4f
Kamron Arabi <kamronarabi@gmail.com>
2025-07-12 10:54:35 -0400

    Transaction table

--- budget.ai/app/(main)/account/[id]/page.jsx
+++ budget.ai/app/(main)/account/[id]/page.jsx
@@ -1,6 +1,8 @@
 import { getAccountWithTransactions } from '@/actions/accounts'
 import { notFound } from 'next/navigation'
-import React from 'react'
+import React, { Suspense } from 'react'
+import TransactionTable from '../_components/transaction-table'
+import { BarLoader } from 'react-spinners'
 
 const AccountsPage = async ({params}) => {
     const accountData= await getAccountWithTransactions(params.id)
@@ -9,7 +11,9 @@ const AccountsPage = async ({params}) => {
     }
     const{transactions, ...account}=accountData
   return (
-    <div className="flex gap-4 items-end justify-between">        <div>
+    <div className='space-y-8 px-5'>
+      <div className="flex gap-4 items-end justify-between">        
+        <div>
             <h1 className="text-5xl sm:text-6xl font-bold bg-gradient-to-br from-blue-600 to-purple-600 font-extrabold tracking-tighter pr-2 pb-2 text-transparent bg-clip-text capitalize">{account.name}</h1>
             <p className='text-muted-foreground'>{account.type.charAt(0)+account.type.slice(1).toLowerCase()} Account</p>
         </div>
@@ -17,10 +21,14 @@ const AccountsPage = async ({params}) => {
         <div className="text-xl sm:text-2xl font-bold">${parseFloat(account.balance).toFixed(2)}</div>
         <p className="text-sm text-muted-foreground">{account._count.transactions} Transactions</p>
         </div>
+       </div>
 
         {/* Chart Section */}
 
         {/* Transaction Table */}
+        <Suspense fallback={<BarLoader className='mt-4' width={'100%'} color='#9333ea'/>}>
+            <TransactionTable transactions={transactions}/>
+        </Suspense>
     </div>
   )
 }
