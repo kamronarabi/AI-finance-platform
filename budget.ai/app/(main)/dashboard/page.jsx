4fac479f24945aa6b013202934b156b038b62d13
Kamron Arabi <kamronarabi@gmail.com>
2025-07-13 12:19:05 -0400

    budget progress bar

--- budget.ai/app/(main)/dashboard/page.jsx
+++ budget.ai/app/(main)/dashboard/page.jsx
@@ -4,12 +4,24 @@ import { Card, CardContent } from '@/components/ui/card'
 import { Plus } from 'lucide-react'
 import React from 'react'
 import AccountCard from './_components/account-card'
+import { getCurrentBudget } from '@/actions/budget'
+import { BudgetProgress } from './_components/budget-progess'
 
 async function DashboardPage(){
 
     const accounts = await getUserAccounts()
-  return <div className='px-5'>
+    const defaultAccount = accounts?.find((account) => account.isDefault);
+    let budgetData = null
+    if(defaultAccount){
+      budgetData = await getCurrentBudget(defaultAccount.id)
+    }
+  return <div className='space-y-8'>
     {/* Budget Progress */}
+    <BudgetProgress
+      initialBudget={budgetData?.budget}
+      currentExpenses={budgetData?.currentExpenses || 0}
+    />
+  
 
     {/* Overview */}
 
